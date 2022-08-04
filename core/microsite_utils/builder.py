d91389cc8344610b72a3a7b3d9215abf35236044
corei8 <corei8.github@gmail.com>
2022-08-04 15:07:43 -0400

    make pages directory if not existing

--- core/microsite_utils/builder.py
+++ core/microsite_utils/builder.py
@@ -1,5 +1,24 @@
-import os
+# import os
+from os import listdir, mkdir
+from re import sub
 
 def get_all_pages():
-	pages = os.listdir('pages/')
+	"""
+	Generates a list of all the markdown pages for the website.
+	"""
+	try:
+		pages = listdir('pages/')
+	except FileNotFoundError:
+		mkdir('./pages/')
+		pages = listdir('pages/')
 	return pages
+
+def check_valid_name(name: str) -> str:
+	"""
+	Ensures that the names for the markdown files will be easy to 
+	work with: no numbers, spaces, special characters, etc.
+	"""
+	if len(name) == 0:
+		valid_name = 'untitled'
+	else: valid_name = name.lower()
+	return valid_name
--- core/routes.py
+++ core/routes.py
@@ -1,11 +1,11 @@
 from datetime import *
 from time import perf_counter
 
-from flask import abort, render_template, request
+from flask import abort, render_template, request, redirect, url_for
 from flask_optimize import FlaskOptimize
 from icecream import ic, install
 
-from core.microsite_utils.builder import get_all_pages
+from core.microsite_utils.builder import get_all_pages, check_valid_name
 
 from core import app
 # from functions import (markdown_checker, render_article, valid_sections,
@@ -35,8 +35,23 @@ PAGES = 'pages/'
 @app.route("/builder_home", strict_slashes=False, methods=['POST', 'GET'])
 @flask_optimize.optimize()
 def builder_home():
+	return render_template(
+			'builder_home.html',
+			pages_list = get_all_pages(),
+			page_view = 'menu', # TODO make this more dynamic
+			)
+
+@app.route("/add_page", strict_slashes=True, methods=['POST', 'GET'])
+def add_page():
 	if request.method == 'POST':
-		# TODO add flag for return type
+		filename = request.form['new_page']
+		valid_filename = check_valid_name(filename)
+		open(PAGES+valid_filename+'.md', 'a').close() # create the file
+		return redirect(url_for('builder_home'))
+
+@app.route("/edit_page", strict_slashes=True, methods=['POST', 'GET'])
+def edit_page():
+	if request.method == 'POST':
 		file = request.form['page_to_edit']
 		with open(PAGES+file, 'r') as f:
 			file_content = f.read()
@@ -44,22 +59,6 @@ def builder_home():
 				'builder_home.html',
 				page_view = 'edit_page',
 				content=file_content
-				)
-	return render_template(
-			'builder_home.html',
-			pages_list = get_all_pages(),
-			page_view = 'menu',
-			)
-
-@app.route("/add_page", strict_slashes=True, methods=['POST', 'GET'])
-def add_page():
-	if request.method == 'POST':
-		filename = request.form['new_page']
-		open(PAGES+filename+'.md', 'a').close() # create the file
-		return render_template(
-				'builder_home.html',
-				pages_list = get_all_pages(),
-				page_view = 'menu'
 				)
 
 # @app.route("/<section>/<string:article>", methods=['GET'], strict_slashes=False)
