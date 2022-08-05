9f28fa7fa42311580095fc1bdbade032a16bada4
corei8 <corei8.github@gmail.com>
2022-08-05 13:45:57 -0400

    add and delete pages

--- core/microsite_utils/builder.py
+++ core/microsite_utils/builder.py
@@ -1,17 +1,7 @@
-# import os
-from os import listdir, mkdir
+from os import listdir, mkdir, remove
 from re import sub
 
-def get_all_pages():
-	"""
-	Generates a list of all the markdown pages for the website.
-	"""
-	try:
-		pages = listdir('pages/')
-	except FileNotFoundError:
-		mkdir('./pages/')
-		pages = listdir('pages/')
-	return pages
+from core.microsite_utils.globals import *
 
 def check_valid_name(name: str) -> str:
 	"""
@@ -22,3 +12,34 @@ def check_valid_name(name: str) -> str:
 		valid_name = 'untitled'
 	else: valid_name = name.lower()
 	return valid_name
+
+def add_markdown_page(filename: str) -> None:
+	""" 
+	Make a file in the pages/ directory with the filename being the 
+	input.
+	"""
+	open(PAGES+check_valid_name(filename)+'.md', 'a').close()
+	return None
+
+def get_all_pages():
+	"""
+	Generates a list of all the markdown pages for the website.
+	"""
+	try:
+		dirty_pages= listdir('pages/')
+	except FileNotFoundError:
+		mkdir('./pages/')
+		dirty_pages = listdir('pages/') # FIXME make this cleaner -> its always len 0
+	if len(dirty_pages) == 0: # make sure that there is a home file
+		add_markdown_page('home')
+		dirty_pages= listdir('pages/')
+	pages = [ page.split('.')[0] for page in dirty_pages ]
+	return pages
+
+
+def delete_page(page: str) -> None:
+	"""
+	Delete the page provided from the pages/ directory.
+	"""
+	remove('pages/'+page+'.md')
+	return None
--- core/routes.py
+++ core/routes.py
@@ -5,7 +5,8 @@ from flask import abort, render_template, request, redirect, url_for
 from flask_optimize import FlaskOptimize
 from icecream import ic, install
 
-from core.microsite_utils.builder import get_all_pages, check_valid_name
+from core.microsite_utils.builder import get_all_pages, check_valid_name, delete_page, add_markdown_page
+from core.microsite_utils.globals import *
 
 from core import app
 # from functions import (markdown_checker, render_article, valid_sections,
@@ -25,29 +26,28 @@ date = datetime.now().strftime('%Y')
 	# return markdown_checker('home', '')
 
 @app.route("/")
-@app.route("/builder", strict_slashes=False)
+@app.route("/login", strict_slashes=False)
 @flask_optimize.optimize()
-def builder():
+def admin_login():
 	return render_template('builder.html')
 
-PAGES = 'pages/'
-
-@app.route("/builder_home", strict_slashes=False, methods=['POST', 'GET'])
+@app.route("/builder", strict_slashes=False, methods=['POST', 'GET'])
 @flask_optimize.optimize()
-def builder_home():
-	return render_template(
-			'builder_home.html',
-			pages_list = get_all_pages(),
-			page_view = 'menu', # TODO make this more dynamic
-			)
-
-@app.route("/add_page", strict_slashes=True, methods=['POST', 'GET'])
-def add_page():
+def builder():
 	if request.method == 'POST':
-		filename = request.form['new_page']
-		valid_filename = check_valid_name(filename)
-		open(PAGES+valid_filename+'.md', 'a').close() # create the file
-		return redirect(url_for('builder_home'))
+		if 'add_page_form' in request.form:
+			filename = request.form['new_page']
+			# TODO add filename validation to page creation
+			valid_filename = check_valid_name(filename)
+			add_markdown_page(valid_filename)
+		elif 'page_to_delete' in request.form:
+			delete_page(request.form['page_to_delete'])
+		else:
+			abort(500)
+	return render_template(
+			'builder/dashboard.html',
+			pages_list = get_all_pages(),
+			)
 
 @app.route("/edit_page", strict_slashes=True, methods=['POST', 'GET'])
 def edit_page():
@@ -56,7 +56,7 @@ def edit_page():
 		with open(PAGES+file, 'r') as f:
 			file_content = f.read()
 		return render_template(
-				'builder_home.html',
+				'builder.html',
 				page_view = 'edit_page',
 				content=file_content
 				)
